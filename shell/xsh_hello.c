f585cb1f209e56ee61e443ba78d378bbbe079cea
jella saikiran <jsaikiran19@gmail.com>
2022-01-18 12:33:58 -0500

    assignment-1 final

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -15,6 +15,16 @@ shellcmd xsh_hello(int nargs, char *args[]) {
 
 	/* Check argument count */
 
+    if (nargs == 2 && strncmp(args[1], "--help", 7) == 0) {
+		printf("Usage: %s\n\n", args[0]);
+		printf("Description:\n");
+		printf("\tDisplays a welcome message\n");
+        printf("Options:\n");
+		printf("\tname...\ttakes exactly one name\n");
+		printf("\t--help\t display this help and exit\n");
+		return 0;
+	}
+
 	if (nargs > 2) {
 		fprintf(stderr, "%s: too many arguments\n", args[0]);
 		fprintf(stderr, "Try '%s --help' for more information\n",
