07e89e16c4ab8ff2f48c78e02f58c6dc3c0d0a9d
jella saikiran <jsaikiran19@gmail.com>
2022-04-24 19:22:12 -0400

    assignment-5

--- system/future.c
+++ system/future.c
@@ -8,12 +8,18 @@ future_t *future_alloc(future_mode_t mode, uint size, uint nelem)
   future->data = sizeof(future_t) + (char *)future;
   future->mode = mode;
   future->state = FUTURE_EMPTY;
-
+  future->get_queue = newqueue();
   return future;
 }
 
 syscall future_free(future_t *future)
 {
+  qid16 q = future->get_queue;
+  while (!isempty(q))
+  {
+    pid32 pid = dequeue(q);
+    kill(pid);
+  }
   return freemem((char *)future, sizeof(future_t) + future->size);
 }
 
@@ -32,7 +38,12 @@ syscall future_set(future_t *future, char *data)
   {
     memcpy(future->data, data, sizeof(data));
     resume(future->pid);
-    future->state = FUTURE_EMPTY;
+     future->state = future->mode==FUTURE_EXCLUSIVE ? FUTURE_EMPTY : FUTURE_READY;
+     qid16 q = future->get_queue;
+    while (!isempty(q))
+    {
+      resume(dequeue(q));
+    }
     return OK;
   }
   return SYSERR;
@@ -45,13 +56,21 @@ syscall future_get(future_t *future, char *data)
     future->state = FUTURE_WAITING;
     future->pid = getpid();
     suspend(future->pid);
+    enqueue(future->pid, future->get_queue);
     memcpy(data, future->data, sizeof(future->data));
     return OK;
   }
   else if (future->state == FUTURE_READY)
   {
     memcpy(data, future->data, sizeof(data));
-    future->state = FUTURE_EMPTY;
+    future->state = future->mode==FUTURE_EXCLUSIVE ? FUTURE_EMPTY : FUTURE_READY;
+    return OK;
+  }
+  else if (future->mode == FUTURE_SHARED)
+  {
+    pid32 pid = getpid();
+    suspend(pid);
+    enqueue(pid, future->get_queue);
     return OK;
   }
 
