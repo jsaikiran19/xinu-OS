8c3d12379eb3927b2bde83fe82f62029ff1250ea
jella saikiran <jsaikiran19@gmail.com>
2022-04-27 17:09:28 -0400

    assignment-5 final

--- system/future.c
+++ system/future.c
@@ -4,27 +4,25 @@
 future_t *future_alloc(future_mode_t mode, uint size, uint nelem)
 {
 
-  future_t *future = (future_t *)getmem(size + sizeof(future_t));
+  int mask = disable();
+  future_t *future = (future_t *)getmem((size) + sizeof(future_t));
   future->data = sizeof(future_t) + (char *)future;
   future->mode = mode;
   future->state = FUTURE_EMPTY;
-  future->get_queue = newqueue();
+  restore(mask);
   return future;
 }
 
 syscall future_free(future_t *future)
 {
-  qid16 q = future->get_queue;
-  while (!isempty(q))
-  {
-    pid32 pid = dequeue(q);
-    kill(pid);
-  }
+  int mask = disable();
+  restore(mask);
   return freemem((char *)future, sizeof(future_t) + future->size);
 }
 
 syscall future_set(future_t *future, char *data)
 {
+  int mask = disable();
 
   if (future->state == FUTURE_EMPTY)
   {
@@ -34,45 +32,34 @@ syscall future_set(future_t *future, char *data)
     return OK;
   }
 
-  else if (future->state == FUTURE_WAITING)
+  if (future->state == FUTURE_WAITING)
   {
     memcpy(future->data, data, sizeof(data));
     resume(future->pid);
-     future->state = future->mode==FUTURE_EXCLUSIVE ? FUTURE_EMPTY : FUTURE_READY;
-     qid16 q = future->get_queue;
-    while (!isempty(q))
-    {
-      resume(dequeue(q));
-    }
+    future->state = FUTURE_EMPTY;
     return OK;
   }
+  restore(mask);
   return SYSERR;
 }
 syscall future_get(future_t *future, char *data)
 {
 
+  int mask = disable();
   if (future->state == FUTURE_EMPTY)
   {
     future->state = FUTURE_WAITING;
     future->pid = getpid();
     suspend(future->pid);
-    enqueue(future->pid, future->get_queue);
     memcpy(data, future->data, sizeof(future->data));
     return OK;
   }
-  else if (future->state == FUTURE_READY)
+  if (future->state == FUTURE_READY)
   {
     memcpy(data, future->data, sizeof(data));
-    future->state = future->mode==FUTURE_EXCLUSIVE ? FUTURE_EMPTY : FUTURE_READY;
+    future->state = FUTURE_EMPTY;
     return OK;
   }
-  else if (future->mode == FUTURE_SHARED)
-  {
-    pid32 pid = getpid();
-    suspend(pid);
-    enqueue(pid, future->get_queue);
-    return OK;
-  }
-
+  restore(mask);
   return SYSERR;
 }
